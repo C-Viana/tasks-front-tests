7c3574d2d23d868b83667915ac28ef18eec69853
Carlos Viana <carlosedsviana@outlook.com>
2022-12-04 14:28:33 -0300

    Alteração da plataforma para execução dos testes

--- src/test/java/tasks/functional/TasksTest.java
+++ src/test/java/tasks/functional/TasksTest.java
@@ -17,26 +17,24 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class TasksTest {
 	
 	public WebDriver getDriver() throws MalformedURLException {
-		if(System.getProperty("webdriver.chrome.driver") == null)
-			System.setProperty("webdriver.chrome.driver", "C:\\Desenvolvimento\\WebDrivers\\chromedriver_108.exe");
-//		WebDriver driver = new ChromeDriver();
 		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setCapability("platformName", "Windows");
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+		chromeOptions.setCapability("platformName", "Linux");
+		chromeOptions.setAcceptInsecureCerts(true);
+		WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/"), chromeOptions);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("http://localhost:8901/tasks/");
+		driver.get("http://192.168.0.103:8901/tasks/");
 		return driver;
 	}
 	
 	@Test
-	public void t01naoDeveCriarTarefaComDataPassada() throws MalformedURLException {
+	public void t01naoDeveCriarTarefaComDataPassada() throws MalformedURLException, InterruptedException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
 			driver.findElement(By.id("task")).sendKeys("Teste funcional");
 			driver.findElement(By.id("dueDate")).sendKeys(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDate.now().minusDays(1)));
 			driver.findElement(By.id("saveButton")).click();
-			
+			Thread.sleep(5000);
 			Assert.assertEquals("Due date must not be in past", driver.findElement(By.id("message")).getText().trim());
 		} finally {
 			driver.quit();
@@ -45,13 +43,13 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void t02naoDeveCriarTarefaSemNome() throws MalformedURLException {
+	public void t02naoDeveCriarTarefaSemNome() throws MalformedURLException, InterruptedException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
 			driver.findElement(By.id("dueDate")).sendKeys(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDate.now().plusDays(2)));
 			driver.findElement(By.id("saveButton")).click();
-			
+			Thread.sleep(5000);
 			Assert.assertEquals("Fill the task description", driver.findElement(By.id("message")).getText().trim());
 		} finally {
 			driver.quit();
@@ -60,13 +58,13 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void t03naoDeveCriarTarefaSemData() throws MalformedURLException {
+	public void t03naoDeveCriarTarefaSemData() throws MalformedURLException, InterruptedException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
 			driver.findElement(By.id("task")).sendKeys("Teste funcional");
 			driver.findElement(By.id("saveButton")).click();
-			
+			Thread.sleep(5000);
 			Assert.assertEquals("Fill the due date", driver.findElement(By.id("message")).getText().trim());
 		} finally {
 			driver.quit();
@@ -75,7 +73,7 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void t04deveCriarTarefaComSucesso() throws MalformedURLException {
+	public void t04deveCriarTarefaComSucesso() throws MalformedURLException, InterruptedException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -83,7 +81,7 @@ public class TasksTest {
 			driver.findElement(By.id("task")).sendKeys(taskDescription);
 			driver.findElement(By.id("dueDate")).sendKeys(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDate.now().plusDays(2)));
 			driver.findElement(By.id("saveButton")).click();
-			
+			Thread.sleep(5000);
 			Assert.assertEquals("Success!", driver.findElement(By.id("message")).getText().trim());
 		} finally {
 			driver.quit();
