28c5b5e24de108d1065c0997edc9ecd7d52f39c5
Carlos Viana <carlosedsviana@outlook.com>
2022-12-03 21:48:36 -0300

    Adição de paralelismo para o selenium grid

--- src/test/java/tasks/functional/TasksTest.java
+++ src/test/java/tasks/functional/TasksTest.java
@@ -1,5 +1,7 @@
 package tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,21 +11,25 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
-	public WebDriver getDriver() {
+	public WebDriver getDriver() throws MalformedURLException {
 		if(System.getProperty("webdriver.chrome.driver") == null)
 			System.setProperty("webdriver.chrome.driver", "C:\\Desenvolvimento\\WebDrivers\\chromedriver_108.exe");
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setCapability("platformName", "Windows");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.56.1:4444"), chromeOptions);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("http://localhost:8901/tasks/");
 		return driver;
 	}
 	
 	@Test
-	public void t01naoDeveCriarTarefaComDataPassada() {
+	public void t01naoDeveCriarTarefaComDataPassada() throws MalformedURLException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -39,7 +45,7 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void t02naoDeveCriarTarefaSemNome() {
+	public void t02naoDeveCriarTarefaSemNome() throws MalformedURLException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -54,7 +60,7 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void t03naoDeveCriarTarefaSemData() {
+	public void t03naoDeveCriarTarefaSemData() throws MalformedURLException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -69,7 +75,7 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void t04deveCriarTarefaComSucesso() {
+	public void t04deveCriarTarefaComSucesso() throws MalformedURLException {
 		WebDriver driver = getDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -85,7 +91,7 @@ public class TasksTest {
 		}
 	}
 	
-//	public void t05deveRemoverTarefaComSucesso() {
+//	public void t05deveRemoverTarefaComSucesso() throws MalformedURLException {
 //		WebDriver driver = getDriver();
 //		driver.findElement(By.xpath("//td[text()='Teste funcional']/following-sibling::td/a")).click();
 //		Assert.assertEquals("Success!", driver.findElement(By.id("message")).getText().trim());
